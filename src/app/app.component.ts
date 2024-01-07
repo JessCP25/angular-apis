57824c025af335ddf2b7a986c7b3e761668c7a14
JessCP25 <jpayanoc25@gmail.com>
2024-01-07 16:57:17 -0500

    Descarga de archivos con http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = '';
   showImg = true;
 
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private filesService: FilesService
+  ) {}
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -21,16 +25,24 @@ export class AppComponent {
   }
 
   createUser() {
-    this.usersService.create({
-      name: 'Jessica',
-      email: 'jessica@gmail.com',
-      password: '12345',
-      avatar: 'https://i.pinimg.com/564x/e9/9b/0a/e99b0a26d6709288ecb8f5ba212305a4.jpg'
-    })
-    .subscribe(rta => {
-      console.log(rta);
-    })
+    this.usersService
+      .create({
+        name: 'Jessica',
+        email: 'jessica@gmail.com',
+        password: '12345',
+        avatar:
+          'https://i.pinimg.com/564x/e9/9b/0a/e99b0a26d6709288ecb8f5ba212305a4.jpg',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
   }
 
-
+  downloadPDF() {
+    this.filesService.getFile(
+      'my.pdf',
+      'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf',
+      'application/pdf'
+    ).subscribe();
+  }
 }
